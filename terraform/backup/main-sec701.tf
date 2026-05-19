9b4617990c2ce6eaa17e4097044e74e67bbd2fa4
washingj777 <washingj77@gmail.com>
2026-05-19 15:33:25 -0400

    add localstack terraform CI/CD lab

--- terraform/backup/main-sec701.tf
+++ terraform/backup/main-sec701.tf
@@ -1,5 +1,7 @@
 terraform {
+
   required_providers {
+
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
@@ -7,13 +9,12 @@ terraform {
   }
 }
 
-variable "localstack_endpoint" {
-  description = "LocalStack endpoint URL"
-  type        = string
-  default     = "http://localhost:4566"
-}
+#
+# AWS Provider for LocalStack
+#
 
 provider "aws" {
+
   access_key = "test"
   secret_key = "test"
 
@@ -27,83 +28,105 @@ provider "aws" {
   s3_use_path_style = true
 
   endpoints {
-    ec2 = var.localstack_endpoint
-    s3  = var.localstack_endpoint
-    iam = var.localstack_endpoint
-    sts = var.localstack_endpoint
+
+    ec2 = "http://localhost:4566"
+    s3  = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    sts = "http://localhost:4566"
   }
 }
 
-# -----------------------------
+#
 # VPC
-# -----------------------------
+#
 
 resource "aws_vpc" "securityplus_vpc" {
+
   cidr_block = "10.10.0.0/16"
 
   tags = {
-    Name        = "securityplus-vpc"
-    Environment = "SecurityPlus-Lab"
+    Name = "securityplus-vpc"
   }
 }
 
-# -----------------------------
-# Subnet
-# -----------------------------
+#
+# Public Subnet
+#
 
 resource "aws_subnet" "public_subnet" {
+
   vpc_id     = aws_vpc.securityplus_vpc.id
   cidr_block = "10.10.1.0/24"
 
   tags = {
-    Name        = "securityplus-public-subnet"
-    Environment = "SecurityPlus-Lab"
+    Name = "public-subnet"
   }
 }
 
-# -----------------------------
+#
 # Security Group
-# -----------------------------
+#
 
 resource "aws_security_group" "web_sg" {
-  name        = "securityplus-web-security-group"
-  description = "Allow web, SSH, and MySQL lab traffic"
+
+  name        = "web-security-group"
+  description = "Allow HTTP and SSH"
   vpc_id      = aws_vpc.securityplus_vpc.id
 
+  #
+  # HTTP
+  #
+
   ingress {
-    description = "Allow HTTP from anywhere for demo purposes"
+
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #
+  # HTTPS
+  #
+
   ingress {
-    description = "Allow HTTPS from anywhere for demo purposes"
+
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #
+  # SSH
+  #
+
   ingress {
-    description = "Allow SSH from anywhere for demo purposes"
+
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #
+  # MySQL
+  #
+
   ingress {
-    description = "Allow MySQL only inside the VPC"
+
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["10.10.0.0/16"]
   }
 
+  #
+  # Outbound
+  #
+
   egress {
-    description = "Allow all outbound traffic"
+
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -111,16 +134,16 @@ resource "aws_security_group" "web_sg" {
   }
 
   tags = {
-    Name        = "securityplus-web-security-group"
-    Environment = "SecurityPlus-Lab"
+    Name = "web-security-group"
   }
 }
 
-# -----------------------------
+#
 # Web Server 1
-# -----------------------------
+#
 
 resource "aws_instance" "webserver1" {
+
   ami           = "ami-12345678"
   instance_type = "t2.micro"
 
@@ -128,17 +151,17 @@ resource "aws_instance" "webserver1" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name        = "webserver1"
-    Role        = "Web Server"
-    Environment = "SecurityPlus-Lab"
+    Name = "webserver1"
+    Role = "Web Server"
   }
 }
 
-# -----------------------------
+#
 # Web Server 2
-# -----------------------------
+#
 
 resource "aws_instance" "webserver2" {
+
   ami           = "ami-12345678"
   instance_type = "t2.micro"
 
@@ -146,17 +169,17 @@ resource "aws_instance" "webserver2" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name        = "webserver2"
-    Role        = "Web Server"
-    Environment = "SecurityPlus-Lab"
+    Name = "webserver2"
+    Role = "Web Server"
   }
 }
 
-# -----------------------------
+#
 # Web Server 3
-# -----------------------------
+#
 
 resource "aws_instance" "webserver3" {
+
   ami           = "ami-12345678"
   instance_type = "t2.micro"
 
@@ -164,17 +187,17 @@ resource "aws_instance" "webserver3" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name        = "webserver3"
-    Role        = "Web Server"
-    Environment = "SecurityPlus-Lab"
+    Name = "webserver3"
+    Role = "Web Server"
   }
 }
 
-# -----------------------------
+#
 # Web Server 4
-# -----------------------------
+#
 
 resource "aws_instance" "webserver4" {
+
   ami           = "ami-12345678"
   instance_type = "t2.micro"
 
@@ -182,17 +205,17 @@ resource "aws_instance" "webserver4" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name        = "webserver4"
-    Role        = "Web Server"
-    Environment = "SecurityPlus-Lab"
+    Name = "webserver4"
+    Role = "Web Server"
   }
 }
 
-# -----------------------------
+#
 # MySQL Database Instance
-# -----------------------------
+#
 
 resource "aws_instance" "mysql_database" {
+
   ami           = "ami-12345678"
   instance_type = "t2.micro"
 
@@ -200,55 +223,22 @@ resource "aws_instance" "mysql_database" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name        = "mysql-database"
-    Role        = "Database Server"
-    Environment = "SecurityPlus-Lab"
+    Name = "mysql-database"
+    Role = "Database"
   }
 }
 
-# -----------------------------
-# S3 Bucket for Log Storage
-# -----------------------------
+#
+# S3 Bucket for Logs
+#
 
 resource "aws_s3_bucket" "log_bucket" {
+
   bucket = "securityplus-log-storage"
 
   tags = {
     Name        = "securityplus-log-storage"
+    Environment = "Lab"
     Purpose     = "Log Storage"
-    Environment = "SecurityPlus-Lab"
   }
-}
-
-# -----------------------------
-# Outputs
-# -----------------------------
-
-output "vpc_id" {
-  value = aws_vpc.securityplus_vpc.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.web_sg.id
-}
-
-output "webserver_names" {
-  value = [
-    aws_instance.webserver1.tags["Name"],
-    aws_instance.webserver2.tags["Name"],
-    aws_instance.webserver3.tags["Name"],
-    aws_instance.webserver4.tags["Name"]
-  ]
-}
-
-output "mysql_database_name" {
-  value = aws_instance.mysql_database.tags["Name"]
-}
-
-output "log_bucket_name" {
-  value = aws_s3_bucket.log_bucket.bucket
 }
